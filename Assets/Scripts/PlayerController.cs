ee6ae813360d75360fcf97c29598aef3c4dd6568
Matt Scorca <mscorca@ucsc.edu>
2018-01-27 12:03:44 -0800

    rudimentary movement, can maxspeed doesn't really work yet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Vector3 _xInput;
+	private Vector3 _zInput;
+	private Rigidbody _rigidBody;
+
+	public float MovementSpeed;
+	public float MaxSpeed;
+
+	protected void Awake() 
+	{
+		_rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	protected void Start () 
+	{
+		
+	}
+	
+	protected void Update () 
+	{
+//		Vector3 inputMovement = new Vector3 ();
+		_xInput.x = Input.GetAxisRaw ("Horizontal") * MovementSpeed;
+		_zInput.z = Input.GetAxisRaw ("Vertical") * MovementSpeed;
+		// _input.y, = 0.0f;
+	}
+
+	protected void FixedUpdate() 
+	{
+
+		if(_rigidBody.velocity.x < MaxSpeed)
+			_rigidBody.AddForce(_xInput, ForceMode.Acceleration);
+
+		if(_rigidBody.velocity.z < MaxSpeed)
+			_rigidBody.AddForce(_zInput, ForceMode.Acceleration);
+
+		Debug.Log("velocity: " + _rigidBody.velocity);
+	}
+}
