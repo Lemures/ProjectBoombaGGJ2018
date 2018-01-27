1c2ac5ea69151b1fa831af75168e0d245132958c
BLURD\Blade <wellwyatt@gmail.com>
2018-01-27 15:49:32 -0800

    mouse movement

--- Assets/Scripts/RatRandomMovement.cs
+++ Assets/Scripts/RatRandomMovement.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class RatRandomMovement : MonoBehaviour {
 
     private GameObject rat;
-    private Rigidbody rb = rat.GetComponent<Rigidbody>();
-    
+    private Rigidbody rb;
+    public float ratVelocity;
+    private float ratRandomness;
+    public float ratRandomMin = .1f;
+    public float ratRandomMax = 1f;
+    private bool ratRunning;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+		if(ratRunning == false) {
+
+        }
 	}
 
+    IEnumerable MoveRat() {
+        ratRunning = true;
+        ratRandomness = Random.Range(ratRandomMin, ratRandomMax);
+        yield return new WaitForSeconds(Random.Range(.1f,1));
+        rb.AddForce(Vector3.forward * ratVelocity, ForceMode.Impulse);
+        ratRunning = false;
+    }
 
 }
