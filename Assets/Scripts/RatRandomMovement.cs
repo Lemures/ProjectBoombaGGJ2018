a113ed2aa8cadf1a6258e3387667187866720adb
BLURD\Blade <wellwyatt@gmail.com>
2018-01-27 15:11:40 -0800

    Falling off, spawner, rat

---
+++ Assets/Scripts/RatRandomMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatRandomMovement : MonoBehaviour {
+
+    private GameObject rat;
+    private Rigidbody rb = rat.GetComponent<Rigidbody>();
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+
+}
