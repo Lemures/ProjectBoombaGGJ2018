8fcc8f899607d1f63a4ed3377b22951a9a25d70a
Matt Scorca <mscorca@ucsc.edu>
2018-01-27 15:36:46 -0800

    added slingshot movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour {
 	private Vector3 _xInput;
 	private Vector3 _zInput;
 	private Rigidbody _rigidBody;
-	private float _launchForce;
+	private float _launchForce = 10.0f;
+	private float _maxLaunchForce = 35.0f;
+	private float _launchCooldown = 2.0f;
+	private float _launchCooldownTimer = 0.0f;
+	private bool _isLaunchOnCooldown;
+	private bool _isLaunching;
+
 
 	public float MovementSpeed;
 	public float MaxSpeed;
@@ -17,26 +23,40 @@ public class PlayerController : MonoBehaviour {
 		_rigidBody = GetComponent<Rigidbody> ();
 	}
 
-	protected void Start () 
-	{
-		
-	}
-	
 	protected void Update () 
 	{
 		_xInput.x = Input.GetAxisRaw ("Horizontal") * MovementSpeed;
 		_zInput.z = Input.GetAxisRaw ("Vertical") * MovementSpeed;
-
-		if(Input.GetButtonDown("Jump")){
-			Debug.Log("JUMP");
-		}
-
-
+		
 		// lookat normalized direction of input
 		Vector3 input = new Vector3(_xInput.x, 0, _zInput.z);
 
 		if(!input.Equals(Vector3.zero))
 			transform.LookAt(transform.position + input.normalized, Vector3.up);
+
+		if(_isLaunchOnCooldown)
+		{
+			if(_launchCooldownTimer < _launchCooldown)
+				_launchCooldownTimer += Time.deltaTime;
+			else 
+				_isLaunchOnCooldown = false;
+		} 
+		else 
+		{
+			if(Input.GetButton("Submit"))
+			{
+				if(_launchForce < _maxLaunchForce)
+					_launchForce += 18.0f * Time.deltaTime;
+			}
+
+			if(Input.GetButtonUp("Submit"))
+			{
+				Debug.Log("LAUNCHEDING: " + _launchForce);
+				_isLaunching = true;
+
+			}
+		}
+
 	}
 
 	protected void FixedUpdate() 
@@ -48,5 +68,13 @@ public class PlayerController : MonoBehaviour {
 		if(Mathf.Abs(_rigidBody.velocity.z) < MaxSpeed)
 			_rigidBody.AddForce(_zInput, ForceMode.Acceleration);
 
+		if(_isLaunching) 
+		{
+			_rigidBody.AddForce(transform.forward * _launchForce, ForceMode.Impulse);
+			_launchForce = 10.0f;
+			_isLaunching = false;
+			_isLaunchOnCooldown = true;
+			_launchCooldownTimer = 0.0f;
+		}
 	}
 }
