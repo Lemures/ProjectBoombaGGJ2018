8f06f9bee78fd66cbdfcc1a23024e3a4fe2bf35c
Matt Scorca <mscorca@ucsc.edu>
2018-01-28 13:56:41 -0800

    timer for game over. need end screen

--- Assets/Scripts/HealthEventManager.cs
+++ Assets/Scripts/HealthEventManager.cs
@@ -5,8 +5,25 @@ using UnityEngine;
 public class HealthEventManager : MonoBehaviour {
 
     public GameObject[] players;
+    public float GameTime = 100.0f;
+
+    private bool _isGameOver;
+
+    protected void Awake() {
+        GameTime = 100.0f;
+    }
 
     void Update() {
+        if(GameTime <= 0 && !_isGameOver) 
+        {
+            GameTime = 0;
+            EndGame();
+            _isGameOver = true;
+        }
+        else if(!_isGameOver){
+            GameTime -= Time.deltaTime;
+        }
+
         foreach (GameObject player in players) {
             var playerHealth = player.GetComponent<PlayerHealth>();
             if(playerHealth.health >= 100) {
@@ -25,4 +42,11 @@ public class HealthEventManager : MonoBehaviour {
             playerHealth.catPowerEnabled = false;
         }
     }
+
+    public void EndGame() 
+    {
+        Debug.Log("GAME OVER!");
+    }
+
+
 }
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+
+	public HealthEventManager GameManager;
+	private Text _timerText;
+
+	void Awake () {
+		_timerText = GetComponent<Text>();
+	}
+	
+	void Update () {
+		string minSec = string.Format("{0}:{1:00}", (int)GameManager.GameTime / 60, (int)GameManager.GameTime % 60); 
+		_timerText.text = minSec;
+	}
+}
