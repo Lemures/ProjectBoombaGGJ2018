dcf01ced8b9032696a154739a0712b8e6cfc56c3
Paul Ambrosiussen <paula@sidefx.com>
2018-01-27 20:29:59 -0800

    Level Edits + Camera Shake

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+
+    static GameObject mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+
+    }
+    public static void CameraShake() {
+        iTween.ShakePosition(mainCamera, iTween.Hash("x", 0.1f, "time", 0.5f));
+    }
+}
