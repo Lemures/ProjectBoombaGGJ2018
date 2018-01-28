679dcd056f7d54487074c3d667471da489bbc086
BLURD\Blade <wellwyatt@gmail.com>
2018-01-27 19:33:30 -0800

    Messing with colliders and damage

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -7,6 +7,7 @@ public class PlayerHealth : MonoBehaviour {
     public float health;
     public float healthIncreaseRate = .1f;
     public LayerMask canDamage;
+    public float playerDamage = 10;
 
 	void Start () {
 	}
@@ -19,12 +20,15 @@ public class PlayerHealth : MonoBehaviour {
         if (health >= 100) {
             health = 100;
         }
+        if (health <= 0) {
+            health = 0;
+        }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.collider.gameObject.layer == 9) {
-            var enemyVelocity = collision.collider.gameObject.GetComponent<Rigidbody>().velocity;
-            health = health-enemyVelocity.magnitude;
+            var enemyVelocity = collision.gameObject.GetComponent<Rigidbody>().velocity;
+            health = health-(enemyVelocity.magnitude * playerDamage);
         }
     }
 }
