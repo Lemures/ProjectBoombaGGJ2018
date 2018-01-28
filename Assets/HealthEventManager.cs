bb914ba0adecc889d33ef2256c1b6ce0bfd58152
BLURD\Blade <wellwyatt@gmail.com>
2018-01-27 18:39:00 -0800

    Player Health

---
+++ Assets/HealthEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthEventManager : MonoBehaviour {
+
+    public GameObject[] players;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.F)) {
+            Reset();
+        }
+    }
+
+    public void Reset(){
+        foreach (GameObject player in players) {
+            var playerHealth = player.GetComponent<PlayerHealth>();
+            playerHealth.health = 0;
+            Debug.Log(playerHealth.health);
+        }
+    }
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float health;
+    public float healthIncreaseRate = .1f;
+    public LayerMask canDamage;
+
+	void Start () {
+	}
+
+    private void FixedUpdate() {
+        health += healthIncreaseRate;
+    }
+
+    private void Update() {
+        if (health >= 100) {
+            health = 100;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.gameObject.layer == 9) {
+            var enemyVelocity = collision.collider.gameObject.GetComponent<Rigidbody>().velocity;
+            health = health-enemyVelocity.magnitude;
+        }
+    }
+}
