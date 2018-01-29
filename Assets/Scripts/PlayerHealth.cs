91e96586c71c353480100483a9988d45c1f281c6
mscorca <matt.scorca@triggerglobal.com>
2018-01-29 12:17:28 -0800

    refactored spawning system to be per player object. only turns off the mesh instead of the entire object

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -17,8 +17,18 @@ public class PlayerHealth : MonoBehaviour
     public GameObject cat;
     public float catScore = 0;
     public GameObject explosionPart;
+    public GameObject BoombaMeshObject;
     private Vector3 explodeLocation;
+    private Rigidbody _rigidBody;
+    public float spawnTime = 1;
+    private float spawnSize = 4.0f;
+    private Vector3 lastPos = new Vector3(50, 50, 50);
+    private bool _isSpawning;
 
+    protected void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -78,22 +88,48 @@ public class PlayerHealth : MonoBehaviour
         {
             AudioController.Instance.PlayRoombaHitSounds();
         }
-        if (collision.collider.gameObject.layer == 8)
+        if (collision.collider.gameObject.layer == 8 && !_isSpawning)
         {
             explodeLocation = gameObject.transform.position;
             StartCoroutine("Explode");
             health = 0;
             AudioController.Instance.PlayRoombaExplosionSounds();
             catPowerEnabled = false;
-            gameObject.SetActive(false);
+            HandleSpawn();
             CameraMovement.CameraShake();
-
         }
     }
-    IEnumerator Explode()
+
+    private void HandleSpawn()
+    {
+        _isSpawning = true;
+        BoombaMeshObject.SetActive(false);
+        _rigidBody.velocity = Vector3.zero;
+        _rigidBody.angularVelocity = Vector3.zero;
+        StartCoroutine("Spawn", gameObject);
+    }
+
+    private IEnumerator Explode()
     {
         Instantiate(explosionPart, explodeLocation, Quaternion.identity);
         yield break;
+    }
+
+    private IEnumerator Spawn(GameObject player)
+    {
+        yield return new WaitForSeconds(spawnTime);
+        player.GetComponent<PlayerHealth>().BoombaMeshObject.SetActive(true);
+        player.transform.eulerAngles = new Vector3(0, 0, 0);
+        Vector3 newPos = Random.insideUnitCircle * spawnSize;
+        //        while (Vector3.Distance(newPos, lastPos) <= .1)
+        //        {
+        //            newPos = Random.insideUnitCircle * spawnSize;
+        //        }
+        newPos.z = newPos.y + 0;
+        newPos.y = 1;
+        player.transform.position = newPos;
+        lastPos = newPos;
+        _isSpawning = false;
     }
 }
 
--- Assets/Scripts/SpawnBoombaAfterFalling.cs
+++ Assets/Scripts/SpawnBoombaAfterFalling.cs
@@ -2,46 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnBoombaAfterFalling : MonoBehaviour {
-
+public class SpawnBoombaAfterFalling : MonoBehaviour
+{
     public float spawnTime = 1;
-    public GameObject[] players;
-    private GameObject currentSpawn;
-    private Vector3 lastPos = new Vector3(50, 50, 50);
     public float spawnSize = 10;
+    private Vector3 lastPos = new Vector3(50, 50, 50);
+    public PlayerHealth[] players;
+    private GameObject currentSpawn;
 
+    void Update()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		foreach(GameObject player in players) {
-            if (player.activeSelf == false) {
-                var rb = player.GetComponent<Rigidbody>();
-                rb.velocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
-                StartCoroutine("Spawn", player);
-            }
-        }
-	}
-
-
-    IEnumerator Spawn(GameObject player) {
-    yield return new WaitForSeconds(spawnTime);
-            if (player.activeSelf == false) {
-                player.SetActive(true);
-                player.transform.eulerAngles = new Vector3(0,0,0);
-                Vector3 newPos = Random.insideUnitCircle * spawnSize;
-                while (Vector3.Distance(newPos, lastPos) <= .1) {
-                    newPos = Random.insideUnitCircle * spawnSize;
-                }
-                newPos.z = newPos.y + transform.position.z;
-                newPos.y = 1;
-                player.transform.position = newPos;
-                lastPos = newPos;
-        }
     }
 }
